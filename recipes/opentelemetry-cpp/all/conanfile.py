3495f3e4b6f9e555720bfe01708d65dd4152228a
ben-alexander.altendorf <ben.altendorf.extern@nexenio.com>
2020-10-27 10:56:52 +0100

    opentelemtry-cpp recipe initial commit

---
+++ recipes/opentelemetry-cpp/all/conanfile.py
@@ -0,0 +1,57 @@
+from conans import ConanFile, CMake, tools
+import os
+
+
+class OpentelemetryCppConan(ConanFile):
+    name = "opentelemetry-cpp"
+    version = "0.0.1"
+    license = "Apache-2.0 License"
+    author = "Ben-A. Altendorf" "b.altendorf@tritime.org"
+    url = "https://github.com/baltendorf/conan-center-index"
+    description = "Cpp implementation of OpenTelemetry"
+    topics = ("opentelemetry", "metrics", "tracing")
+    settings = "os", "compiler", "build_type", "arch"
+    exports_sources = ["CMakeLists.txt"]
+    options = {"shared": [True, False]}
+    default_options = {"shared": False}
+    generators = "cmake"
+
+    _cmake = None
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    def source(self):
+        self.run("git clone -b test/cmake_install https://github.com/baltendorf/opentelemetry-cpp.git")
+        os.rename(self.name, self._source_subfolder)
+
+    def _configure_cmake(self):
+        if self._cmake:
+            return self._cmake
+
+        self._cmake = CMake(self)
+        self._cmake.definitions["WITH_TESTS"] = False
+        self._cmake.definitions["WITH_EXAMPLES"] = False
+
+        self._cmake.configure(source_folder=self._source_subfolder)
+        return self._cmake
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def package(self):
+        cmake = self._configure_cmake()
+        cmake.install()
+
+        #self.copy("*.h", dst="include", src=)
+        #self.copy("*hello.lib", dst="lib", keep_path=False)
+        #self.copy("*.dll", dst="bin", keep_path=False)
+        #self.copy("*.so", dst="lib", keep_path=False)
+        #self.copy("*.dylib", dst="lib", keep_path=False)
+        #self.copy("*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
+
---
+++ recipes/opentelemetry-cpp/all/test_package/TestPackage.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+#include <opentelemetry/sdk/trace/simple_processor.h>
+#include <opentelemetry/sdk/trace/tracer_provider.h>
+#include <opentelemetry/trace/provider.h>
+#include <opentelemetry/exporters/ostream/span_exporter.h>
+
+int main(int argc, char *argv[])
+{
+  auto exporter = std::unique_ptr<sdktrace::SpanExporter>(
+      new opentelemetry::exporter::trace::OStreamSpanExporter);
+  auto processor = std::shared_ptr<sdktrace::SpanProcessor>(
+      new sdktrace::SimpleSpanProcessor(std::move(exporter)));
+  auto provider = nostd::shared_ptr<opentelemetry::trace::TracerProvider>(
+      new sdktrace::TracerProvider(processor));
+  
+  // Set the global trace provider
+  opentelemetry::trace::Provider::SetTracerProvider(provider);    
+
+  return 0;
+}
